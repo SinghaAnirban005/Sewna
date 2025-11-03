b186e8c6494075e928751ca4ac1f546cceb773a2
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-11-03 20:48:51 +0530

    add pages for landing page

---
+++ client/sewna/src/pages/Index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Header";
+import { SplitHero } from "../components/SplitHero";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <SplitHero />
+    </main>
+  );
+};
+
+export default Index;
