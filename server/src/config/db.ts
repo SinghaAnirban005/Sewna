8e1442b9797ad2c3ba2dc9c834a63d68941116da
SinghaAnirban005 <anirbansingha20@gmail.com>
2025-11-04 03:11:23 +0530

    add db init config

---
+++ server/src/config/db.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+export const connectDatabase = async (): Promise<void> => {
+  try {
+    const mongoUri = String(process.env.MONGODB_URI);
+
+    console.log(mongoUri)
+    
+    await mongoose.connect(mongoUri);
+    console.log('Connected to MongoDB successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
